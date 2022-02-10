4e2a7ea04d10626014ea83455873736c8cc86305
AjayKumar049 <92783730+AjayKumar049@users.noreply.github.com>
2022-02-10 07:01:08 -0500

    Add files via upload

---
+++ Assignment18/script.js
@@ -0,0 +1,36 @@
+/*Adding Queryselector and EventListener to Button1 & Declaring the var ie audio*/
+document.querySelector(".btn1").addEventListener("click",function(){
+	var audio1 = new Audio("ride.wav");
+	audio1.play();
+	
+})
+/*Adding Queryselector and EventListener to Button2 & Declaring the var ie audio*/
+document.querySelector(".btn2").addEventListener("click",function(){
+	var audio1 = new Audio("crash.mp3");
+	audio1.play();
+	
+})
+/*Adding Queryselector and EventListener to Button3 & Declaring the var ie audio*/
+document.querySelector(".btn3").addEventListener("click",function(){
+	var audio1 = new Audio("sounds_kick-bass.mp3");
+	audio1.play();
+	
+})
+/*Adding Queryselector and EventListener to Button4 & Declaring the var ie audio*/
+document.querySelector(".btn4").addEventListener("click",function(){
+	var audio1 = new Audio("hihat.wav");
+	audio1.play();
+	
+})
+/*Adding Queryselector and EventListener to Button5 & Declaring the var ie audio*/
+document.querySelector(".btn5").addEventListener("click",function(){
+	var audio1 = new Audio("kick.wav");
+	audio1.play();
+	
+})
+/*Adding Queryselector and EventListener to Button6 & Declaring the var ie audio*/
+document.querySelector(".btn6").addEventListener("click",function(){
+	var audio1 = new Audio("tink.wav");
+	audio1.play();
+	
+})
