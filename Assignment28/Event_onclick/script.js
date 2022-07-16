96e172501bd07988192d758eb0a0b050c8d5e7d2
AjayKumar049 <92783730+AjayKumar049@users.noreply.github.com>
2022-07-16 04:47:29 -0400

    Add files via upload

---
+++ Assignment28/Event_onclick/script.js
@@ -0,0 +1,14 @@
+function playhorse(){
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+function playtiger(){
+    var audio = document.getElementById("sit");
+    audio.play();
+}
+function playelephant(){
+    var audio = document.getElementById("demo");
+    audio.play();
+}
+
